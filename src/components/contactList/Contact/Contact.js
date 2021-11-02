c17877af4501a4916f0dbd61bdf71f69d4a2cd45
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2021-11-02 13:11:13 +0330

    added Link coponent for Edit button

--- src/components/contactList/Contact/Contact.js
+++ src/components/contactList/Contact/Contact.js
@@ -4,9 +4,7 @@ const Contact = ({ contact, onDelete }) => {
   const { name, email, id } = contact;
   return (
     <div key={id} className="item">
-      <div
-        style={{ display: "flex", alignItems: "center" }}
-      >
+      <div style={{ display: "flex", alignItems: "center" }}>
         <BiUserCircle className="userIcon" />
         <Link to={{ pathname: `user/${id}`, state: { contact } }}>
           <div className="user">
@@ -15,8 +13,13 @@ const Contact = ({ contact, onDelete }) => {
           </div>
         </Link>
       </div>
+      <div>
+        <Link to={`/edit/${id}`}>
+          <button className="editBtn">Edit</button>
+        </Link>
 
-      <button onClick={() => onDelete(id)}>Delete </button>
+        <button onClick={() => onDelete(id)}>Delete </button>
+      </div>
     </div>
   );
 };
